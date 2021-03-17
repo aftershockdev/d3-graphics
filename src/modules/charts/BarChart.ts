5c036af14470d4d275b4068e8f37b7e4f9796a6b
Ruslan Zherebytskyi <rusikmvp@gmail.com>
2021-03-18 00:10:17 +0200

    add dev branch

--- src/modules/charts/BarChart.ts
+++ src/modules/charts/BarChart.ts
@@ -95,142 +95,142 @@ export default class BarChart implements BarInterface {
       .attr("height", (d: BarFunc) => this.height - yscale(d.value))
       .attr("width", xscale.bandwidth() - 3);
 
-    // if (isScrollDisplayed) {
-    //   let xOverview = d3
-    //     .scaleBand()
-    //     .domain(
-    //       data.map(function (d) {
-    //         return d.label;
-    //       })
-    //     )
-    //     .range([0, this.width]);
+    if (isScrollDisplayed) {
+      let xOverview = d3
+        .scaleBand()
+        .domain(
+          data.map(function (d) {
+            return d.label;
+          })
+        )
+        .range([0, this.width]);
 
-    //   let yOverview = d3.scaleLinear().range([heightOverview, 0]);
-    //   yOverview.domain(yscale.domain());
+      let yOverview = d3.scaleLinear().range([heightOverview, 0]);
+      yOverview.domain(yscale.domain());
 
-    //   let subBars = diagram.selectAll(".subBar").data(data);
+      let subBars = diagram.selectAll(".subBar").data(data);
 
-    //   subBars
-    //     .enter()
-    //     .append("rect")
-    //     .classed("subBar", true)
-    //     .attr(<any>{
-    //       height: (d: BarFunc) => heightOverview - yOverview,
-    //       width: (d: BarFunc) => xOverview.bandwidth(),
-    //       x: (d: any) => xOverview(d.date),
-    //       y: (d: BarFunc) => this.height + heightOverview + yOverview(d.value),
-    //     });
+      subBars
+        .enter()
+        .append("rect")
+        .classed("subBar", true)
+        .attr(<any>{
+          height: (d: BarFunc) => heightOverview - yOverview,
+          width: (d: BarFunc) => xOverview.bandwidth(),
+          x: (d: any) => xOverview(d.date),
+          y: (d: BarFunc) => this.height + heightOverview + yOverview(d.value),
+        });
 
-    //   var displayed = d3
-    //     .scaleQuantize()
-    //     .domain([0, this.width])
-    //     .range(d3.range(data.length));
+      var displayed = d3
+        .scaleQuantize()
+        .domain([0, this.width])
+        .range(d3.range(data.length));
 
-    //   diagram
-    //     .append("rect")
-    //     .attr(
-    //       "transform",
-    //       "translate(0, " + (this.height + this.margin.bottom) + ")"
-    //     )
-    //     .attr("class", "mover")
-    //     .attr("x", 0)
-    //     .attr("y", 0)
-    //     .attr("height", selectorHeight)
-    //     .attr(
-    //       "width",
-    //       Math.round(parseFloat(numBars * this.width) / data.length)
-    //     )
-    //     .attr("pointer-events", "all")
-    //     .attr("cursor", "ew-resize")
-    //     .call(d3.drag().on("drag", display));
+      diagram
+        .append("rect")
+        .attr(
+          "transform",
+          "translate(0, " + (this.height + this.margin.bottom) + ")"
+        )
+        .attr("class", "mover")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("height", selectorHeight)
+        .attr(
+          "width",
+          Math.round(parseFloat(numBars * this.width) / data.length)
+        )
+        .attr("pointer-events", "all")
+        .attr("cursor", "ew-resize")
+        .call(d3.drag().on("drag", display));
 
-    //   this.svg.on("scroll", display);
-    // }
-    // function display() {
-    //   let x = parseInt(d3.select(this).attr("x")),
-    //     nx = x + d3.event.dx,
-    //     w = parseInt(d3.select(this).attr("width")),
-    //     f,
-    //     nf,
-    //     new_data,
-    //     rects;
+      this.svg.on("scroll", display);
+    }
+    function display() {
+      let x = parseInt(d3.select(this).attr("x")),
+        nx = x + d3.event.dx,
+        w = parseInt(d3.select(this).attr("width")),
+        f,
+        nf,
+        new_data,
+        rects;
 
-    //   if (nx < 0 || nx + w > this.width) return;
+      if (nx < 0 || nx + w > this.width) return;
 
-    //   d3.select(this).attr("x", nx);
+      d3.select(this).attr("x", nx);
 
-    //   f = displayed(x);
-    //   nf = displayed(nx);
+      f = displayed(x);
+      nf = displayed(nx);
 
-    //   if (f === nf) return;
+      if (f === nf) return;
 
-    //   new_data = data.slice(nf, nf + numBars);
+      new_data = data.slice(nf, nf + numBars);
 
-    //   xscale.domain(
-    //     new_data.map(function (d) {
-    //       return d.label;
-    //     })
-    //   );
-    //   diagram.select(".x.axis").call(d3.axisBottom(xscale));
+      xscale.domain(
+        new_data.map(function (d) {
+          return d.label;
+        })
+      );
+      diagram.select(".x.axis").call(d3.axisBottom(xscale));
 
-    //   rects = bars.selectAll("rect").data(new_data, function (d) {
-    //     return d.label;
-    //   });
+      rects = bars.selectAll("rect").data(new_data, function (d) {
+        return d.label;
+      });
 
-    //   label = labels.selectAll(".label").data(new_data, function (d) {
-    //     return d.label;
-    //   });
+      label = labels.selectAll(".label").data(new_data, function (d) {
+        return d.label;
+      });
 
-    //   rects.attr("x", function (d) {
-    //     return xscale(d.label);
-    //   });
-    //   label.attr("x", function (d) {
-    //     return xscale(d.label);
-    //   });
+      rects.attr("x", function (d) {
+        return xscale(d.label);
+      });
+      label.attr("x", function (d) {
+        return xscale(d.label);
+      });
 
-    //   rects
-    //     .enter()
-    //     .append("rect")
-    //     .attr("class", "bar")
-    //     .attr("x", function (d) {
-    //       return xscale(d.label);
-    //     })
-    //     .attr("y", function (d) {
-    //       return yscale(d.value);
-    //     })
-    //     .attr("width", xscale.bandwidth())
-    //     .attr("height", function (d) {
-    //       return this.height - yscale(d.value);
-    //     });
+      rects
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", function (d) {
+          return xscale(d.label);
+        })
+        .attr("y", function (d) {
+          return yscale(d.value);
+        })
+        .attr("width", xscale.bandwidth())
+        .attr("height", function (d) {
+          return this.height - yscale(d.value);
+        });
 
-    //   label
-    //     .enter()
-    //     .append("text")
-    //     .attr("class", "label")
-    //     .attr("x", function (d) {
-    //       return xscale(d.label);
-    //     })
-    //     .style("fill", (d) => {
-    //       return d.value ===
-    //         d3.max(data, (d) => {
-    //           return d.value;
-    //         })
-    //         ? "red"
-    //         : "red";
-    //     })
-    //     .attr("y", function (d) {
-    //       return yscale(d.value) - 5;
-    //     })
-    //     .text((d) => {
-    //       return d.value;
-    //     })
-    //     .attr("width", xscale.bandwidth())
-    //     .attr("height", function (d) {
-    //       return this.height - yscale(d.value);
-    //     });
+      label
+        .enter()
+        .append("text")
+        .attr("class", "label")
+        .attr("x", function (d) {
+          return xscale(d.label);
+        })
+        .style("fill", (d) => {
+          return d.value ===
+            d3.max(data, (d) => {
+              return d.value;
+            })
+            ? "red"
+            : "red";
+        })
+        .attr("y", function (d) {
+          return yscale(d.value) - 5;
+        })
+        .text((d) => {
+          return d.value;
+        })
+        .attr("width", xscale.bandwidth())
+        .attr("height", function (d) {
+          return this.height - yscale(d.value);
+        });
 
-    //   label.exit().remove();
-    //   rects.exit().remove();
-    // }
+      label.exit().remove();
+      rects.exit().remove();
+    }
   }
 }
